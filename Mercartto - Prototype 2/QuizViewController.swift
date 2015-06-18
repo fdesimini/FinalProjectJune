8410670cda9939cfb88af675299c632856f6c05f
fdesimini <fdesimini@users.noreply.github.com>
2015-06-17 22:16:26 -0400

    Adding Quiz feature - finished laying out UI

---
+++ Mercartto - Prototype 2/QuizViewController.swift
@@ -0,0 +1,38 @@
+//
+//  QuizViewController.swift
+//  Mercartto - Prototype 2
+//
+//  Created by Frank Desimini on 2015-06-17.
+//  Copyright (c) 2015 Eff Dee Productions. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    @IBAction func noPreference(sender: AnyObject) {
+    }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
